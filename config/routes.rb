d0dc6b219d6cd731eb8db1d20d72a68c529aa797
pauline_barnades <barnades.pauline@gmail.com>
2020-04-29 10:21:08 +0200

    adding v1 route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-namespace :api do
-  namespace :v0 do
-    resources :pings, only: [:index], constraints: { format: 'json'}
+  mount_devise_token_auth_for 'User', at: 'ap i/v1/auth', skip: [:omniauth_callbacks]
+  namespace :api do
+    namespace :v0 do
+      resources :pings, only: [:index], constraints: { format: 'json'}
+    end
+    
+    namespace :v1 do
   end
  end
 end
