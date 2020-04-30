9e6a62349aa4c7126a5cfc3ab3040070f2fcdfd3
Erik Björn <erik.g.bjoern@gmail.com>
2020-04-30 12:31:21 +0200

    formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
-# frozen_string_literal: true
-
 class User < ActiveRecord::Base
   extend Devise::Models
+  
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
+  
   include DeviseTokenAuth::Concerns::User
+  
   has_many :performance_data, class_name: 'PerformanceData'
-
 end
