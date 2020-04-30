d5435cd04cf523eabe3da36c3868af21de04d734
Erik Björn <erik.g.bjoern@gmail.com>
2020-04-30 12:33:23 +0200

    ...formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   extend Devise::Models
   
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
   
   include DeviseTokenAuth::Concerns::User
   
